fe8ca51fb859030dad8ac77ace63857747ce66c4
brad_d <badefauw@gmail.com>
2021-06-01 21:05:50 -0500

    initial js commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const guessedLetters = document.querySelector(".guessed-letters");
+const guess = document.querySelector(".guess");
+const letter = document.querySelector(".letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const remaining = document.querySelector(".remaining");
+const remainingGuesses = document.querySelector("span");
+const message = document.querySelector(".message");
+const playAgain = document.querySelector(".play-again");
+const word = "magnolia";
+
+const placeholder = function(word){
+    const placeholderLetters = [];
+    for(const letter of word){
+        console.log(letter);
+        placeholderLetters.push("●");
+    }
+    wordInProgress.innerText = placeholderLetters.join("");
+}
+
+placeholder(word);
+
+guess.addEventListener("click", function(e){
+    e.preventDefault();
+    const input = letter.value;
+    console.log(input);
+});
